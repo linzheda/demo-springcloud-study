65d99eb146d117bbaa0dd9b460d8b83f0b7a00f8
lzd <717537314@qq.com>
2018-01-26 09:55:21 +0800

    springcloud 学习

--- microservice-provider-student/src/main/java/com/yc/ProviderApp.java
+++ microservice-provider-student/src/main/java/com/yc/ProviderApp.java
@@ -2,15 +2,17 @@ package com.yc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lzd
  * @date 2018/01/17
  *
  */
+@EnableEurekaClient
 @SpringBootApplication
-public class App {
+public class ProviderApp {
     public static void main( String[] args ) {
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(ProviderApp.class,args);
     }
 }
